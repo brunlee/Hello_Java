fc712f8c28f5a8de61b23227cdd32e8a90462115
brunlee <73282857+brunlee@users.noreply.github.com>
2020-10-23 17:03:38 +0200

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
-      System.out.println("Hello, World modificado");
+      System.out.println("Hello, World modificado... otra vez");
    }
 }
